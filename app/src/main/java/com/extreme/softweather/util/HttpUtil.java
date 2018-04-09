6ab2513a8461af855f6fa1d0bdb57a7425349082
SoftSquirrel <1690027871@qq.com>
2018-04-09 20:13:17 +0800

    get districts

---
+++ app/src/main/java/com/extreme/softweather/util/HttpUtil.java
@@ -0,0 +1,14 @@
+package com.extreme.softweather.util;
+
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String url, Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(url).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
