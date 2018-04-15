6618f9aaf221c17d5143ef1fddc7c42a49acff2e
SoftSquirrel <1690027871@qq.com>
2018-04-15 09:25:07 +0800

    done

--- app/src/main/java/com/extreme/softweather/util/Utility.java
+++ app/src/main/java/com/extreme/softweather/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.extreme.softweather.db.City;
 import com.extreme.softweather.db.County;
 import com.extreme.softweather.db.Province;
+import com.extreme.softweather.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -68,5 +70,20 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
